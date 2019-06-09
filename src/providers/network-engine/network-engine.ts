7d2b6543fc4a7516d23999a3b391c153be19bb62
Jose Ignacio Santa Cruz G <j@santa.cl>
2019-06-09 00:00:17 -0400

    CHG Corrige modo de acceso a API para poder acceder desde una PWA

--- src/providers/network-engine/network-engine.ts
+++ src/providers/network-engine/network-engine.ts
@@ -1,4 +1,4 @@
-import { Http }       from '@angular/http'; 
+import { Http }       from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/map';
@@ -8,38 +8,40 @@ import 'rxjs/add/operator/toPromise';
 export class NetworkEngineProvider {
 
   // url:    string = "http://23.239.29.171";  /* servidor linode mio */
-  url:    string = "http://45.56.70.121";  /* servidor linode cliente*/
-  puerto: string = "3020";                  /* puerto: ZSMOTOR */  
+  // url:    string = "http://45.56.70.121";  /* servidor linode cliente*/
+  url:    string = "https://api.kinetik.cl/zspwa";  /* servidor linode cliente*/
+  // puerto: string = "3020";                  /* puerto: ZSMOTOR */
+  puerto: string = "443";                  /* puerto: ZSMOTOR */
 
   constructor( public http: Http ) {
-      console.log('<<< NetworkengineProvider >>>');  
+      console.log('<<< NetworkengineProvider >>>');
   }
 
   soloEnviarCorreo( pCarro, cTo, cCc, cTextoObs )  {
     console.log("soloEnviarCorreo()->",pCarro);
     let accion = "/soloEnviarCorreo";
-    let url    = this.url +":"+ this.puerto + accion;
+    let url    = this.url  + accion;
     let body   = { carro: pCarro, cTo: cTo, cCc: cCc, cObs: cTextoObs };
-    return this.http.post( url, body ).map( res => res.json() );  
+    return this.http.post( url, body ).map( res => res.json() );
   }
 
   sendMail( rutocorreo: string ) {
     let accion = "/sendmail";
-    let url    = this.url +":"+ this.puerto + accion;
+    let url    = this.url  + accion;
     let body   = { rutocorreo: rutocorreo };
     return this.http.post( url, body ).map( res => res.json() );
   }
 
   traeUnSP( cSP: string, parametros?: any, pUser?: any ) {
-    let accion = "/proalma"; 
-    let url    = this.url +":"+ this.puerto + accion;
+    let accion = "/proalma";
+    let url    = this.url + accion;
     let body   = { sp: cSP, datos: parametros, user: pUser };
     return this.http.post( url, body ).map( res => res.json() );
   }
 
   rescataSeteoCliente() {
-    let accion = "/seteocliente"; 
-    let url    = this.url +":"+ this.puerto + accion;
+    let accion = "/seteocliente";
+    let url    = this.url  + accion;
     let body   = { x: 'ktp_configuracion' };
     return this.http.post( url, body ).map( res => res.json() );
   }
@@ -47,9 +49,9 @@ export class NetworkEngineProvider {
   grabarDocumentos( pCarro, pModalidad, cTipodoc, cTextoObs, cTextoOcc )  {
     console.log("grabadocumentos()->",pCarro);
     let accion = "/grabadocumentos";
-    let url    = this.url +":"+ this.puerto + accion;
+    let url    = this.url  + accion;
     let body   = { carro: pCarro, modalidad: pModalidad, tipodoc: cTipodoc, cObs: cTextoObs, cOcc: cTextoOcc };
-    return this.http.post( url, body ).map( res => res.json() );  
+    return this.http.post( url, body ).map( res => res.json() );
   }
-  
+
 }
